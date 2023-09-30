840bbbc92260a97bc70f51b9ba1fb69493db98ab
Oruchan Asar <oruchan.asar@gmail.com>
2023-09-30 12:27:30 +0300

    add podcasts

--- app/bookmarks/podcasts/page.js
+++ app/bookmarks/podcasts/page.js
@@ -1,3 +1,51 @@
 export default function Podcasts() {
-  return <div>Enter</div>;
+  return (
+    <div className="flex flex-wrap justify-between gap-6">
+      <iframe
+        style={{ borderRadius: "12px" }}
+        src="https://open.spotify.com/embed/show/2ySVrxGkN6n6frMTo9Nsrt?utm_source=generator"
+        width="100%"
+        height="352"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+      <iframe
+        style={{ borderRadius: "12px" }}
+        src="https://open.spotify.com/embed/show/3imVa0uYZnmgrqKHj3o8Sq?utm_source=generator"
+        width="100%"
+        height="352"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+      <iframe
+        style={{ borderRadius: "12px" }}
+        src="https://open.spotify.com/embed/show/6HCwhbKxFO9URWPrvf6vs0?utm_source=generator"
+        width="100%"
+        height="352"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+      <iframe
+        style={{ borderRadius: "12px" }}
+        src="https://open.spotify.com/embed/show/5gaAD2SPBy7XY9188rXufJ?utm_source=generator"
+        width="100%"
+        height="352"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+      <iframe
+        style={{ borderRadius: "12px" }}
+        src="https://open.spotify.com/embed/show/5NbxzMRbuun0SIOh7GKMbk?utm_source=generator"
+        width="100%"
+        height="352"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+    </div>
+  );
 }
